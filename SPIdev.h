1ef7ecd06215b1657cc734488c81826e65928105
jangsungLee <hoan12340@naver.com>
2019-02-27 01:09:21 +0900

    Update SPIdev.h

--- SPIdev.h
+++ SPIdev.h
@@ -11,7 +11,13 @@
 #include <linux/spi/spidev.h>  
 
 static const char *device = "/dev/spidev0.0";
-static uint8_t _spi_mode;//=0;
+static uint8_t _spi_mode=1;//=0;
+/* mode   CPOL(Clock Ploarity)    CPHA(Clock Phase)   
+   0 :           0                     0
+   1 :           0                     1
+   2 :           1                     0
+   3 :           1                     1
+*/
 static uint8_t _spi_bits = 8;
 static uint32_t _spi_speed = 1000000;// 1MHz(I designed for the sensor of 'ads1118'. - page.18)
 static uint16_t _spi_delay = 0;
